5f47b408b4acad11867cf5ef68526245899d902e
Ashraf Amer <ashraf.amer55@gmail.com>
2022-02-26 07:34:36 +0200

    new: add routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, Application } from 'express';
 import bodyParser from 'body-parser';
 import userRouter from './api/controllers/UserController';
+import categoryRouter from './api/controllers/CategoryController';
+import productRouter from './api/controllers/ProductController';
+import orderRouter from './api/controllers/OrderController';
 import cors from 'cors';
 
 const app: Application = express();
@@ -10,6 +13,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/users', userRouter);
+app.use('/categories', categoryRouter);
+app.use('/products', productRouter);
+app.use('/orders', orderRouter);
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!');
