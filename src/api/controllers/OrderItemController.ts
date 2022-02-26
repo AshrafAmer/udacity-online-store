d0fa2f7a5f7854ba54ae4aef9980254765c87efd
Ashraf Amer <ashraf.amer55@gmail.com>
2022-02-26 07:35:05 +0200

    fix: maybe compine order items inside order

--- src/api/controllers/OrderItemController.ts
+++ src/api/controllers/OrderItemController.ts
@@ -3,33 +3,10 @@ import { OrderItem } from '../models/OrderItem';
 import { OrderItem as OrderItemType } from '../types/OrderItem'; 
 import { RequiredParamsIdError } from '../../errors/RequiredParamsIdError';
 
+// maybe don't need it
 const orderItemRouter = express.Router();
 const orderItemInstance = OrderItem.createObject() as OrderItem;
 
-orderItemRouter.get(
-    '/',
-    async (req: Request, res: Response) => {
-        const items = await orderItemInstance.index();
-        res.json(items);
-    }
-);
-
-orderItemRouter.get(
-    '/:id',
-    async (req: Request, res: Response) => {
-        const id = req.params.id;
-        if ( !id ) {
-            res.send(new RequiredParamsIdError('order items'));
-        }
-
-        try {
-            const item = await orderItemInstance.show(id);
-            res.status(200).json(item);
-        } catch ( err ) {
-            res.status(400).send(err);
-        }
-    }
-);
 
 orderItemRouter.post(
     '/',
