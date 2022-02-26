85b67ee56ef577ebb3952eeec1925e0975ac034e
Ashraf Amer <ashraf.amer55@gmail.com>
2022-02-26 05:39:47 +0200

    user: create, show, and index

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -1,13 +1,50 @@
 import express, {Request, Response} from 'express';
+import { RequiredParamsIdError } from '../../errors/RequiredParamsIdError';
 const userRouter = express.Router();
 import { User } from './../models/User'; 
+import { User as UserType } from './../types/User'; 
+
+const userInstance = User.createObject() as User;
 
 userRouter.get(
     '/',
     async (req: Request, res: Response) => {
-        console.log('here');
-        const users = await new User().index();
+        const users = await userInstance.index();
         res.json(users);
+    }
+);
+
+userRouter.get(
+    '/:id',
+    async (req: Request, res: Response) => {
+        const id = req.params.id;
+        if ( !id ) {
+            res.send(new RequiredParamsIdError('users'));
+        }
+
+        try {
+            const user = await userInstance.show(id);
+            res.status(200).json(user);
+        } catch ( err ) {
+            res.status(400).send(err);
+        }
+    }
+);
+
+userRouter.post(
+    '/',
+    async (req: Request, res: Response) => {
+        const { firstName, lastName, password } = req.body;
+        
+        try {
+            const userDate: UserType = { firstName, lastName, password };
+            await userInstance.validate(userDate);
+            await userInstance.createUser(userDate);
+            const user = await userInstance.create();
+            res.status(200).json(user);
+        } catch ( err ) {
+            res.status(400).send(err);
+        }
     }
 );
 
