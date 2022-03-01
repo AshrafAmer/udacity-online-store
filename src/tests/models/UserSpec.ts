ecc4269e8841b54d4cedc934a8b8ac5b042b900e
Ashraf Amer <ashraf.amer55@gmail.com>
2022-03-01 05:43:40 +0200

    test: test model

--- src/tests/models/UserSpec.ts
+++ src/tests/models/UserSpec.ts
@@ -1,15 +1,44 @@
-import {User as UserType} from './../../api/types/User';
 import {User} from './../../api/models/User';
 
-const user = new User();
+const user = User.createObject() as User;
 
 describe("User Model Tests", () => {
+
     it("should have an index method", () => {
         expect(user.index).toBeDefined();
     });
 
     it("index method should return list of users", async() => {
         const result = await user.index();
-        expect(result).toEqual([]);
+        expect(result.length).toEqual(3);
     });
+
+    it("show method should return users data", async() => {
+        const result = await user.show(1);
+        expect(result.id).toEqual(1);
+    });
+
+    it("create method should create new user", async() => {
+        user.createUser({
+            firstName: 'testing user',
+            lastName: 'from jasmine',
+            username: 'test@jasmine',
+            password: '123456',
+        });
+        const result = await user.create();
+        expect(result.username).toEqual('test@jasmine');
+    });
+
+    it("delete method should delete user data", async() => {
+        // before delete
+        const users = await user.index();
+        const usersCount = users.length;
+        const result = await user.delete(users[2].id);
+
+        // after delete
+        const usersAfterDelete = await user.index();
+        const usersCountAfterDelete = usersAfterDelete.length;
+        expect(usersCount).toEqual(usersCountAfterDelete + 1);
+    });
+
 });
