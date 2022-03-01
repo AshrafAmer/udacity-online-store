cd07d56a4e7a42f445507d00521d999f9264a073
Ashraf Amer <ashraf.amer55@gmail.com>
2022-03-01 05:42:55 +0200

    add test env database

--- src/database/database.ts
+++ src/database/database.ts
@@ -8,13 +8,27 @@ const {
     POSTGRES_DATABASE,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
+    TEST_POSTGRES_HOST,
+    TEST_POSTGRES_DATABASE,
+    TEST_POSTGRES_USER,
+    TEST_POSTGRES_PASSWORD,
+    ENV,
 } = process.env;
 
-const client = new Pool({
+let client =  new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_DATABASE,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
 });
+
+if (ENV === 'test') {
+    client = new Pool({
+        host: TEST_POSTGRES_HOST,
+        database: TEST_POSTGRES_DATABASE,
+        user: TEST_POSTGRES_USER,
+        password: TEST_POSTGRES_PASSWORD,
+    });
+}
 
 export default client;
